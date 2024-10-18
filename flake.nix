560d744ba899255167c4e3924bf9e1e2f2a38779
demyxology <spicycoldnoodles@gmail.com>
2024-10-18 00:53:37 -0400

    add x220 support, added hostname lol

--- flake.nix
+++ flake.nix
@@ -1,12 +1,15 @@
 {
+
   inputs = {
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
     nur = { url = "github:nix-community/NUR"; };
   };
 
-  outputs = { self, nixpkgs, nur }: {
-    nixosConfigurations.joes-desktop = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, nur, nixos-hardware }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [ 
+        nixos-hardware.lenovo-thinkpad-x220
         ./hardware-configuration.nix
         ./configuration.nix 
       ];
